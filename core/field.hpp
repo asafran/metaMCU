66bbc0cf892fda96eb5a8c016a0b25f13a196ff6
asafran <git@github.com>
2024-12-19 14:36:13 +0300

    Operators implemented

--- core/field.hpp
+++ core/field.hpp
@@ -2,6 +2,7 @@
 #define FIELD_HPP
 
 #include <concepts>
+#include <bit>
 #include <cstddef>
 
 #include "register.hpp"
@@ -24,6 +25,31 @@ namespace metaMCU::core {
         Register::bit_band_clear(0x0);
     };
 
+    template<typename Field, Field::Value_t Value>
+    class Field_value
+    {
+    public:
+        [[gnu::always_inline]] static void set()
+        {
+            Field::set(Value);
+        }
+
+        [[gnu::always_inline]] inline static bool is_set()
+        {
+            return Field::get() == Value;
+        }
+
+        template<typename FV1, typename FV2>
+        consteval auto operator|(this FV1 lhs, FV2 rhs)
+        {
+            constexpr auto offset = 0;
+            constexpr auto mask = lhs.mask_with_offset() | rhs.mask_with_offset();
+            //constexpr bool canRead = lhs.a;
+            //constexpr Field<F1::Register_t, offset, mask, F1::Access_t>
+            return rhs;
+        }
+    };
+
     /*!
      * \brief Обеспечивает безопасный доступ к битовым полям регистров
      * микроконтроллера
@@ -37,74 +63,78 @@ namespace metaMCU::core {
      * \tparam size Размер поля в бит
      * \tparam Access Тип доступа (WriteMode, ReadMode или ReadWriteMode)
      */
-    template<typename Register, size_t Offset, size_t Size, typename Access>
+    template<typename Register, Register::Value_t Offset, Register::Value_t Mask, typename Access>
     class Field : protected Register
     {
     public:
         using Value_t = typename Register::Value_t;
 
-        /// \brief Смещение поля в бит от 0
-        static consteval auto bit_offset()
-        {
-            return Offset;
-        }
-
-        /// \brief Размер поля в бит
-        static consteval auto size()
-        {
-            return Size;
-        }
-
-        /// \brief Маска битового поля
-        static consteval auto mask()
-        {
-            return ((1 << size()) - 1) << bit_offset();
-        }
-
         /// \brief Записывает значение в битовое поле регистра, если регистр позволяет запись
-        template<typename Value>
+        template<typename T = void>
             requires Can_read<Access> && Can_write<Access>
-        [[gnu::always_inline]] inline static void set()
+        [[gnu::always_inline]] inline static void set(Value_t value)
         {
-            //Register::template values_set<Value>();
+            auto register_value = Register::read();
+
+            register_value &= ~mask_with_offset();
+            register_value |= (value << Offset);
+
+            Register::write(register_value);
         }
 
         /// \brief Записывает значение в битовое поле регистра, если регистр позволяет запись
-        template<typename Value>
+        template<typename T = void>
             requires Can_write<Access>
-        [[gnu::always_inline]] inline static void write()
+        [[gnu::always_inline]] inline static void write(Value_t value)
         {
-            Register::template values_write<Value>();
+            static_assert(value_in_range(value));
+            dynamic_write(value);
         }
 
         /// \brief Возвращает значение битового поля регистра
         template<typename Value>
             requires Can_read<Access>
-        [[gnu::always_inline]] inline static Register::Value_t is_set()
+        [[gnu::always_inline]] inline static Register::Value_t get()
         {
-            Register::template values_is_set<Value>();
-        }
-    };
-
-    template<typename Field, Field::Value_t Value>
-    class Field_value : public Field
-    {
-    public:
-        /// \brief Значение битового поля без смещения
-        static consteval auto value()
-        {
-            return Value;
+            return (Register::read() & mask_with_offset) >> Offset;
         }
 
-        [[gnu::always_inline]] static void set()
+        template<typename F1, typename R1, R1::Value_t O1, R1::Value_t M1, typename A1, typename F2>
+        consteval auto operator|(this F1<R1, O1> lhs, F2 rhs)
         {
-            Field::template set<Field_value>();
+            constexpr auto offset = 0;
+            constexpr auto mask = lhs.mask_with_offset() | rhs.mask_with_offset();
+            //constexpr bool canRead = lhs.a;
+            //constexpr Field<F1::Register_t, offset, mask, F1::Access_t>
+            return rhs;
         }
 
-        [[gnu::always_inline]] inline static bool is_set()
+    protected:
+/*        using Register_t = Register;
+        using Access_t = Access;*/
+
+        static consteval bool value_in_range(Value_t value)
         {
-            return Field::template is_set<Field_value>();
+            return (value & ~mask_with_offset()) == 0;
         }
+
+        static consteval auto bit_offset()
+        {
+            return Offset;
+        }
+
+        static consteval auto size()
+        {
+            return std::popcount(Mask);
+        }
+
+        static consteval auto mask_with_offset()
+        {
+            return Mask << Offset;
+        }
+
+        template<typename F, F::Value_t V>
+        friend class Field_value;
     };
 }
 
